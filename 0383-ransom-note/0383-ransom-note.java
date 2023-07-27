75356178eb0acaa1dc459a1eeebe4c49755d2fbb
Kyung Soo "Andy" Min <counterchord89@gmail.com>
2023-07-27 19:00:26 +0900

    Time: 1 ms (100.00%), Space: 43.8 MB (69.58%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.java
+++ 0383-ransom-note/0383-ransom-note.java
@@ -12,19 +12,30 @@ class Solution {
         //     return !matcher.find();
         // }
         // return false;
-        HashMap<Character, Integer> result = new HashMap<>();
-        for(char c : ransomNote.toCharArray()){
-            result.put(c,result.getOrDefault(c,0)+1);
-        }
-        for(char c : magazine.toCharArray()){
-            if(result.containsKey(c)){
-                result.put(c,result.getOrDefault(c,0)-1);
-            }
-        }
-        for(int val : result.values()){
-            if(val > 0){
-                return false;
-            }
+        // HashMap<Character, Integer> result = new HashMap<>();
+        // for(char c : ransomNote.toCharArray()){
+        //     result.put(c,result.getOrDefault(c,0)+1);
+        // }
+        // for(char c : magazine.toCharArray()){
+        //     if(result.containsKey(c)){
+        //         result.put(c,result.getOrDefault(c,0)-1);
+        //     }
+        // }
+        // for(int val : result.values()){
+        //     if(val > 0){
+        //         return false;
+        //     }
+        // }
+        // return true;
+        if (ransomNote.length() > magazine.length()) return false;
+        int[] alphabets_counter = new int[26];
+        
+        for (char c : magazine.toCharArray())
+            alphabets_counter[c-'a']++;
+
+        for (char c : ransomNote.toCharArray()){
+            if (alphabets_counter[c-'a'] == 0) return false;
+            alphabets_counter[c-'a']--;
         }
         return true;
     }

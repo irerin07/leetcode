6ef63306f9f525743b4f9d4a85c661c2eb1f9237
Kyung Soo "Andy" Min <counterchord89@gmail.com>
2023-07-31 04:05:55 +0900

    Time: 3 ms (57.42%), Space: 44.4 MB (7.51%) - LeetHub

--- 1089-duplicate-zeros/1089-duplicate-zeros.java
+++ 1089-duplicate-zeros/1089-duplicate-zeros.java
@@ -3,21 +3,16 @@ class Solution {
     List <Integer> ll = new ArrayList();
 
     int index=0;
-    //index increment as we add elements in List
 
-    for(int i=0;i<arr.length;i++) {
-
-      if(arr[i]==0)
-      {
-        ll.add(index++,arr[i]);      // zero from array
-        ll.add(index++,0);           // add duplicate zero 
+    for(int i=0; i<arr.length; i++) {
+      if(arr[i]==0) {
+        ll.add(index++,arr[i]);      
+        ll.add(index++,0);          
+      } else { 
+        ll.add(index++,arr[i]); 
       }
 
-      else
-      { ll.add(index++,arr[i]); }
-
       arr[i]=ll.get(i);
-      // we will update our Arrays form 0 , with respect to our Updated list
     }
   }
 }

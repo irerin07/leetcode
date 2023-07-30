73bad2f5d1e244fc1494bf8a6cad5409d95c4906
Kyung Soo "Andy" Min <counterchord89@gmail.com>
2023-07-31 04:30:14 +0900

    Time: 0 ms (100.00%), Space: 41.2 MB (78.05%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.java
+++ 0088-merge-sorted-array/0088-merge-sorted-array.java
@@ -1,9 +1,22 @@
+// class Solution {
+//     public void merge(int[] nums1, int m, int[] nums2, int n) {
+//         for (int j = 0, i = m; j < n; j++) {
+//             nums1[i] = nums2[j];
+//             i++;
+//         }
+//         Arrays.sort(nums1);
+//     }
+// }
 class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
-        for (int j = 0, i = m; j < n; j++) {
-            nums1[i] = nums2[j];
-            i++;
+        int p1 = m-1 , p2 = n-1 ,i = m+n-1;
+        while(p2 >=0 ){
+            if(p1 >=0 && nums1[p1] > nums2[p2]){
+                nums1[i--] = nums1[p1--];
+            } 
+            else{
+                nums1[i--] = nums2[p2--];
+            }
         }
-        Arrays.sort(nums1);
-    }
+       }
 }

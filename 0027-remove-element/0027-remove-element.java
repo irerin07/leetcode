879d7a438c81a01c405062b907364c2d22e586f2
Kyung Soo "Andy" Min <counterchord89@gmail.com>
2023-08-01 20:51:51 +0900

    Time: 0 ms (100.00%), Space: 41.2 MB (27.87%) - LeetHub

--- 0027-remove-element/0027-remove-element.java
+++ 0027-remove-element/0027-remove-element.java
@@ -1,14 +1,14 @@
 class Solution {
- public int removeElement(int[] nums, int val) {
-        int i = 0;
-        for (int j = 0; j < nums.length; j++) {
-            if (nums[j] != val) {
-                int temp = nums[i];
-                nums[i] = nums[j];
-                nums[j] = temp;
-                i++;
-            }
-        }
-        return i;
+public int removeElement(int[] nums, int val) {
+    int answer = 0;
+    for (int i = 0; i < nums.length; i++) {
+      if (nums[i] != val) {
+        int temp = nums[answer];
+        nums[answer] = nums[i];
+        nums[i] = temp;
+        answer++;
+      }
     }
+    return answer;
+  }
 }

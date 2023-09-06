d7c191f2fecb95fba8372a0e1d9347336788a818
Kyung Soo "Andy" Min <counterchord89@gmail.com>
2023-09-06 14:26:51 +0900

    Time: 1 ms (53.43%), Space: 50.6 MB (71.35%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.java
+++ 0344-reverse-string/0344-reverse-string.java
@@ -1,17 +1,33 @@
+// class Solution {
+//     public void reverseString(char[] s) {
+//         Stack<Character> st = new Stack<>();
+//         String str = new String(s);
+//         for(int i = 0; i < str.length(); i++){
+//             st.push(s[i]);
+//         }
+//         char ans[] = new char[s.length];
+//         int i = 0;
+//         while(st.size() > 0){
+//             s[i++] = st.pop();
+//         }
+//         for(int j = 0; j < str.length(); j++){
+//             ans[j] = str.charAt(j);
+//         }
+//     }
+// }
+
 class Solution {
     public void reverseString(char[] s) {
-        Stack<Character> st = new Stack<>();
-        String str = new String(s);
-        for(int i = 0; i < str.length(); i++){
-            st.push(s[i]);
-        }
-        char ans[] = new char[s.length];
         int i = 0;
-        while(st.size() > 0){
-            s[i++] = st.pop();
-        }
-        for(int j = 0; j < str.length(); j++){
-            ans[j] = str.charAt(j);
+        int j = s.length - 1;
+        
+        while(i <= j){
+            char temp = s[i];
+            s[i] = s[j];
+            s[j] = temp;
+            i++;
+            j--;
         }
     }
 }
+

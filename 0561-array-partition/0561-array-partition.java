e9dab2b0ce1faa0991e722af09d7ab8c3f7c52a7
Kyung Soo "Andy" Min <counterchord89@gmail.com>
2023-09-30 19:32:00 +0900

    Time: 12 ms (95.42%), Space: 45 MB (60.25%) - LeetHub

--- 0561-array-partition/0561-array-partition.java
+++ 0561-array-partition/0561-array-partition.java
@@ -1,10 +1,12 @@
 class Solution {
     public int arrayPairSum(int[] nums) {
-        int max = 0;
+            
+        int sum = 0;
         Arrays.sort(nums);
-        for(int i = 0;i<nums.length;i = i+2){
-            max = max + Math.min(nums[i],nums[i+1]);
-        }
-        return max;
+            
+        for(int i=0; i<nums.length; i=i+2) 
+            sum += nums[i];
+        
+        return sum;
     }
 }

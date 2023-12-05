c1edbbcc78be462c79bc87674ca4f4f03c79d94c
Kyung Soo "Andy" Min <counterchord89@gmail.com>
2023-12-05 17:14:02 +0900

    Time: 2 ms (87.60%), Space: 61.3 MB (86.75%) - LeetHub

--- 0111-minimum-depth-of-binary-tree/0111-minimum-depth-of-binary-tree.java
+++ 0111-minimum-depth-of-binary-tree/0111-minimum-depth-of-binary-tree.java
@@ -14,29 +14,38 @@
  * }
  */
 class Solution {
-public int minDepth(TreeNode root) {
-    if (root == null) {
-        return 0;
-    }
-    Queue<TreeNode> queue = new LinkedList<>();
-    queue.offer(root);
-    int level = 1;
-    while (!queue.isEmpty()) {
-        int size = queue.size();
-        for (int i = 0; i < size; i++) {
-            TreeNode curNode = queue.poll();
-            if (curNode.left == null && curNode.right == null) {
-                return level;
-            }
-            if (curNode.left != null) {
-                queue.offer(curNode.left);
-            }
-            if (curNode.right != null) {
-                queue.offer(curNode.right);
-            }
+    public int minDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
         }
-        level++;
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        
+        int level = 1;
+        
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            
+            for (int i = 0; i < size; i++) {
+                TreeNode curNode = queue.poll();
+                
+                if (curNode.left == null && curNode.right == null) {
+                    return level;
+                }
+                
+                if (curNode.left != null) {
+                    queue.offer(curNode.left);
+                }
+                
+                if (curNode.right != null) {
+                    queue.offer(curNode.right);
+                }
+                
+            }
+            
+            level++;
+        }
+        
+        return level;
     }
-    return level;
-}
 }

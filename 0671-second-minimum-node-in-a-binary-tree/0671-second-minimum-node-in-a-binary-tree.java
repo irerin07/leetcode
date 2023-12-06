e7383012e97dfcdfbd51b1f338417f8e0eda898a
Kyung Soo "Andy" Min <counterchord89@gmail.com>
2023-12-07 02:45:02 +0900

    Time: 0 ms (100.00%), Space: 40.7 MB (6.55%) - LeetHub

--- 0671-second-minimum-node-in-a-binary-tree/0671-second-minimum-node-in-a-binary-tree.java
+++ 0671-second-minimum-node-in-a-binary-tree/0671-second-minimum-node-in-a-binary-tree.java
@@ -14,36 +14,59 @@
  * }
  */
 class Solution {
-    public int findSecondMinimumValue(TreeNode root) {
-        
-        if (root == null) {
-            return -1;
-        }
-        
-        Stack<TreeNode> stack = new Stack<>();
-        
-        TreeNode current = root;
-        Set<Integer> set = new LinkedHashSet<>();
-        
-        while(current != null || !stack.isEmpty()) {
-            while (current != null) {
-                stack.push(current);
-                set.add(current.val);
-                current = current.left;
+        public int findSecondMinimumValue(TreeNode root) {
+
+        Integer[] ans = new Integer[1];
+
+        inOrderTraversal(root, root.val, ans);
+
+        return ans[0] != null ? ans[0] : -1;
+
+    }
+
+    public void inOrderTraversal(TreeNode node, int rootVal, Integer[] ans) {
+
+        if (node.left != null) {
+
+            //left and right both equals root
+            if (node.left.val == node.right.val && node.left.val == rootVal) {
+                inOrderTraversal(node.left, node.left.val, ans);
+                inOrderTraversal(node.right, node.right.val, ans);
+                return;
             }
-            
-            current = stack.pop();
-            set.add(current.val);
-            current = current.right;
+
+            //left equals root
+            if (node.left.val == rootVal) {
+                ans[0] = findMin(node.right.val, ans[0]);
+                inOrderTraversal(node.left, node.left.val, ans);
+                return;
+            }
+
+            //right equals root
+            if (node.right.val == rootVal) {
+                ans[0] = findMin(node.left.val, ans[0]);
+                inOrderTraversal(node.right, node.right.val, ans);
+                return;
+            }
+
+            //left and right different from root
+            if (node.left.val < node.right.val) {
+                ans[0] = findMin(node.left.val, ans[0]);
+                // return;
+            } else {
+                ans[0] = findMin(node.right.val, ans[0]);
+                // return;
+            }
         }
-        
-        if (set.size() < 2) {
-            return -1;
+    }
+
+    public int findMin(Integer a, Integer b) {
+        if (b == null) {
+            return a;
+        } else if (a == null) {
+            return b;
+        } else {
+            return Math.min(a, b);
         }
-        
-        List<Integer> list = new ArrayList<>(set); 
-        Collections.sort(list);
-        
-        return list.get(1);
     }
 }

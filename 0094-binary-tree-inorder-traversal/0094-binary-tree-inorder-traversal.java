459318859bc97d7c5bc665d468d847e9e8723fc6
Kyung Soo "Andy" Min <counterchord89@gmail.com>
2023-12-08 17:48:23 +0900

    Time: 0 ms (100.00%), Space: 41 MB (25.53%) - LeetHub

--- 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.java
+++ 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.java
@@ -17,25 +17,41 @@ class Solution {
     public List<Integer> inorderTraversal(TreeNode root) {
         List<Integer> answer = new ArrayList<>();
         
-        Stack<TreeNode> stack = new Stack<>();
+        // Stack<TreeNode> stack = new Stack<>();
         
         if (root == null) {
             return answer;
         }
         
-        TreeNode current = root;
+//         TreeNode current = root;
         
-        while(current != null || !stack.isEmpty()) {
-            while (current != null) {
-                stack.push(current);
-                current = current.left;
-            }
+//         while(current != null || !stack.isEmpty()) {
+//             while (current != null) {
+//                 stack.push(current);
+//                 current = current.left;
+//             }
             
-            current = stack.pop();
-            answer.add(current.val);
-            current = current.right;
-        }
+//             current = stack.pop();
+//             answer.add(current.val);
+//             current = current.right;
+//         }
+        
+//         return answer;
+        helper(root, answer);
         
         return answer;
+    
+    }
+    
+    public void helper(TreeNode node, List<Integer> answer) {
+        if (node.left != null) {
+            helper(node.left, answer);
+        }
+        
+        answer.add(node.val);
+            
+        if (node.right != null) {
+            helper(node.right, answer);
+        }
     }
 }

7899efb67bb644eb1be7c854aaeff22eced04984
Kyung Soo "Andy" Min <counterchord89@gmail.com>
2023-12-08 17:49:57 +0900

    Time: 0 ms (100.00%), Space: 40.8 MB (46.82%) - LeetHub

--- 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.java
+++ 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.java
@@ -16,29 +16,43 @@
 class Solution {
     public List<Integer> postorderTraversal(TreeNode root) {
 	    LinkedList<Integer> ans = new LinkedList<>();
-	    Stack<TreeNode> stack = new Stack<>();
+	    // Stack<TreeNode> stack = new Stack<>();
         
 	    if (root == null) { 
             return ans;
         }
         
-	    stack.push(root);
+// 	    stack.push(root);
         
-	    while (!stack.isEmpty()) {
-		    TreeNode cur = stack.pop();
-		    ans.addFirst(cur.val);
+// 	    while (!stack.isEmpty()) {
+// 		    TreeNode cur = stack.pop();
+// 		    ans.addFirst(cur.val);
             
-		    if (cur.left != null) {
-			    stack.push(cur.left);
-		    }
+// 		    if (cur.left != null) {
+// 			    stack.push(cur.left);
+// 		    }
             
-		    if (cur.right != null) {
-			    stack.push(cur.right);
-		    } 
+// 		    if (cur.right != null) {
+// 			    stack.push(cur.right);
+// 		    } 
             
-	    }
+// 	    }
+        
+        helper(root, ans);
         
 	    return ans;
+    }
+    
+    public void helper(TreeNode node, LinkedList<Integer> ans) {
+        if (node.left != null) {
+            helper(node.left, ans);
+        }
+        
+        if (node.right != null) {
+            helper(node.right, ans);
+        }
+        
+        ans.add(node.val);
     }
     
 }

6162937d6afd35e4b7cf819efe8d532efd9f569a
Kyung Soo "Andy" Min <counterchord89@gmail.com>
2024-02-21 19:55:09 +0900

    Time: 0 ms (100.00%), Space: 41 MB (75.69%) - LeetHub

--- 1732-find-the-highest-altitude/1732-find-the-highest-altitude.java
+++ 1732-find-the-highest-altitude/1732-find-the-highest-altitude.java
@@ -1,17 +1,33 @@
+// class Solution {
+//     public int largestAltitude(int[] gain) {
+//         int answer = 0;
+
+//         int[] ints = new int[gain.length + 1];
+//         ints[0] = 0;
+
+//         for (int i = 1; i < ints.length; i++) {
+//             ints[i] = gain[i - 1] + ints[i-1];
+//             if (answer < ints[i]) {
+//                 answer = ints[i];
+//             }
+//         }
+
+//         return answer;
+//     }
+// }
 class Solution {
-    public int largestAltitude(int[] gain) {
-        int answer = 0;
+  public int largestAltitude(int[] gain) {
+    int currentAltitude = 0;
+    // Highest altitude currently is 0.
+    int highestPoint = currentAltitude;
 
-        int[] ints = new int[gain.length + 1];
-        ints[0] = 0;
-
-        for (int i = 1; i < ints.length; i++) {
-            ints[i] = gain[i - 1] + ints[i-1];
-            if (answer < ints[i]) {
-                answer = ints[i];
-            }
-        }
-
-        return answer;
+    for (int altitudeGain : gain) {
+      // Adding the gain in altitude to the current altitude.
+      currentAltitude += altitudeGain;
+      // Update the highest altitude.
+      highestPoint = Math.max(highestPoint, currentAltitude);
     }
+
+    return highestPoint;
+  }
 }

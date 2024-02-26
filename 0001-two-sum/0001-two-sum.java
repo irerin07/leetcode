58c5c568cd77eccd9787dcdc87f1ccf298a6c275
Kyung Soo "Andy" Min <counterchord89@gmail.com>
2024-02-26 20:40:33 +0900

    Time: 2 ms (98.13%), Space: 44.6 MB (87.25%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,28 +1,43 @@
+// class Solution {
+//     public int[] twoSum(int[] nums, int target) {
+//       Map<Integer, Integer> map = new HashMap<>();
+
+//         int index = 0;
+
+//         for (int num : nums) {
+//             map.put(num, index);
+//             index += 1;
+//         }
+
+//         int[] ints = new int[2];
+//         for (int i = 0; i < nums.length; i++) {
+//             int i1 = target - nums[i];
+//             if (map.get(i1) != null && map.get(i1) != i) {
+//                 ints[0] = i;
+//                 ints[1] = map.get(i1);
+//                 break;
+//             }
+//         }
+
+//         for (int anInt : ints) {
+//             System.out.println("anInt = " + anInt);
+//         }
+
+//         return ints;
+//     }
+// }
+
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-      Map<Integer, Integer> map = new HashMap<>();
-
-        int index = 0;
-
-        for (int num : nums) {
-            map.put(num, index);
-            index += 1;
-        }
-
-        int[] ints = new int[2];
+        Map<Integer, Integer> map = new HashMap<>();
         for (int i = 0; i < nums.length; i++) {
-            int i1 = target - nums[i];
-            if (map.get(i1) != null && map.get(i1) != i) {
-                ints[0] = i;
-                ints[1] = map.get(i1);
-                break;
-            }
+            int remaining = target - nums[i];
+            if (map.containsKey(remaining)) {
+                return new int[] { 
+                    map.get(remaining), i };
+                }
+            map.put(nums[i], i);
         }
-
-        for (int anInt : ints) {
-            System.out.println("anInt = " + anInt);
-        }
-
-        return ints;
+        throw new IllegalArgumentException("No two sum solution");
     }
 }

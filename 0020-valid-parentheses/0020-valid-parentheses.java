1d7c89345b0dd396c9fe20c932b3f765ac331717
Kyung Soo "Andy" Min <counterchord89@gmail.com>
2024-02-26 20:51:28 +0900

    Time: 2 ms (58.52%), Space: 41.3 MB (68.09%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,52 +1,70 @@
+// class Solution {
+//     public boolean isValid(String s) {
+// Stack<Character> stack = new Stack<>();
+//         Stack<Character> stack2 = new Stack<>();
+
+//         char[] charArray = s.toCharArray();
+//         if (charArray[0] == '}' || charArray[0] == ')' || charArray[0] == ']' ) {
+//             return false;
+//         }
+
+//         for (char c : charArray) {
+//             if (c == '{' || c == '(' || c == '[' ) {
+//                 stack.push(c);
+//             }
+
+//             if (c == '}') {
+//                 if (stack.isEmpty()) {
+//                     return false;
+//                 }
+//                 if (stack.peek() == '{') {
+//                     stack.pop();
+//                 } else {
+//                     stack2.push(c);
+//                 }
+//             }
+
+//             if (c == ')') {
+//                 if (stack.isEmpty()) {
+//                     return false;
+//                 }
+//                 if (stack.peek() == '(') {
+//                     stack.pop();
+//                 } else {
+//                     stack2.push(c);
+//                 }
+//             }
+
+//             if ( c == ']' ) {
+//                 if (stack.isEmpty()) {
+//                     return false;
+//                 }
+//                 if (stack.peek() == '[') {
+//                     stack.pop();
+//                 } else {
+//                     stack2.push(c);
+//                 }
+//             }
+//         }
+
+//         return stack.isEmpty() && stack2.isEmpty();
+//     }
+// }
+
 class Solution {
     public boolean isValid(String s) {
-Stack<Character> stack = new Stack<>();
-        Stack<Character> stack2 = new Stack<>();
-
-        char[] charArray = s.toCharArray();
-        if (charArray[0] == '}' || charArray[0] == ')' || charArray[0] == ']' ) {
-            return false;
+        Stack<Character> stack = new Stack<Character>(); 
+        for (char c : s.toCharArray()) { 
+            if (c == '(') 
+                stack.push(')'); 
+            else if (c == '{')
+                stack.push('}'); 
+            else if (c == '[') 
+                stack.push(']'); 
+            else if (stack.isEmpty() || stack.pop() != c)
+                return false;
         }
 
-        for (char c : charArray) {
-            if (c == '{' || c == '(' || c == '[' ) {
-                stack.push(c);
-            }
-
-            if (c == '}') {
-                if (stack.isEmpty()) {
-                    return false;
-                }
-                if (stack.peek() == '{') {
-                    stack.pop();
-                } else {
-                    stack2.push(c);
-                }
-            }
-
-            if (c == ')') {
-                if (stack.isEmpty()) {
-                    return false;
-                }
-                if (stack.peek() == '(') {
-                    stack.pop();
-                } else {
-                    stack2.push(c);
-                }
-            }
-
-            if ( c == ']' ) {
-                if (stack.isEmpty()) {
-                    return false;
-                }
-                if (stack.peek() == '[') {
-                    stack.pop();
-                } else {
-                    stack2.push(c);
-                }
-            }
-        }
-
-        return stack.isEmpty() && stack2.isEmpty();
+        return stack.isEmpty();
     }
 }

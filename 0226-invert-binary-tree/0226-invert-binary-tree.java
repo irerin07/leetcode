de1e01862e922d58349b2c0824da90083cdc440f
Kyung Soo "Andy" Min <counterchord89@gmail.com>
2024-02-29 00:47:06 +0900

    Time: 0 ms (100.00%), Space: 41.1 MB (52.56%) - LeetHub

--- 0226-invert-binary-tree/0226-invert-binary-tree.java
+++ 0226-invert-binary-tree/0226-invert-binary-tree.java
@@ -15,18 +15,20 @@
  */
 class Solution {
     public TreeNode invertTree(TreeNode root) {
-        // Base case: if the tree is empty...
-        if(root == null){
+        if (root == null) {
             return root;
         }
-        // Call the function recursively for the left subtree...
+        
         invertTree(root.left);
-        // Call the function recursively for the right subtree...
         invertTree(root.right);
-        // Swapping process...
-        TreeNode curr = root.left;
+    
+        TreeNode temp = root.left;
+        
         root.left = root.right;
-        root.right = curr;
-        return root;        // Return the root...
+        root.right = temp;     
+        
+        return root;
     }
+
+    
 }

4941ea97fb838179b71acb3b6aa8d1bd9ce87d32
Kyung Soo "Andy" Min <counterchord89@gmail.com>
2024-02-28 23:34:44 +0900

    Time: 15 ms (28.81%), Space: 44.6 MB (43.85%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,31 +1,24 @@
 class Solution {
-    public boolean isPalindrome(String s) {
-    
-        int startPosition = 0;
-        int lastPosition = s.length() -1;
-        
-        while (startPosition <= lastPosition) {
-            char currFirst = s.charAt(startPosition);
-            char currLast = s.charAt(lastPosition);
-            
-            if (!Character.isLetterOrDigit(currFirst)) {
-                startPosition++;
-                continue;
-            }
-            
-            if (!Character.isLetterOrDigit(currLast)) {
-                lastPosition--;
-                continue;
-            }
-            
-            if (Character.toLowerCase(currFirst) != Character.toLowerCase(currLast)) {
-        		return false;
-        	}
-            
-            startPosition++;
-            lastPosition--;
+    public static boolean isPalindrome(String s) {
+        String lowerCase = s.replaceAll(
+                "[^a-zA-Z0-9]", "").toLowerCase();
+
+        if (lowerCase.isBlank()) {
+            return true;
         }
-        
+
+        int head = 0;
+        int tail = lowerCase.length() - 1;
+
+        while (head < tail) {
+            if (lowerCase.charAt(head) != lowerCase.charAt(tail)) {
+                return false;
+            }
+
+            head += 1;
+            tail -= 1;
+        }
+
         return true;
     }
 }

d86f45ec82e3a137dffd5b3f3d4fc46210459f21
Kyung Soo "Andy" Min <counterchord89@gmail.com>
2024-03-05 23:02:48 +0900

    Time: 0 ms (100.00%), Space: 44.2 MB (74.63%) - LeetHub

--- 0110-balanced-binary-tree/0110-balanced-binary-tree.java
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.java
@@ -15,25 +15,33 @@
  */
 class Solution {
     public boolean isBalanced(TreeNode root) {
-        // If the tree is empty, we can say it’s balanced...
-        if (root == null)  return true;
-        // Height Function will return -1, when it’s an unbalanced tree...
-		if (Height(root) == -1)  return false;
-		return true;
-	}
-    // Create a function to return the “height” of a current subtree using recursion...
-	public int Height(TreeNode root) {
-        // Base case...
-		if (root == null)  return 0;
-        // Height of left subtree...
-		int leftHeight = Height(root.left);
-        // Height of height subtree...
-		int rightHight = Height(root.right);
-        // In case of left subtree or right subtree unbalanced, return -1...
-		if (leftHeight == -1 || rightHight == -1)  return -1;
-        // If their heights differ by more than ‘1’, return -1...
-        if (Math.abs(leftHeight - rightHight) > 1)  return -1;
-        // Otherwise, return the height of this subtree as max(leftHeight, rightHight) + 1...
-		return Math.max(leftHeight, rightHight) + 1;
+        if (root == null) {
+            return true;
+        }
+        
+        if (isBalancedHelper(root) == -1) {
+            return false;
+        }
+        
+        return true;
+    }
+    
+    public int isBalancedHelper(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        
+        int left = isBalancedHelper(root.left);
+        int right = isBalancedHelper(root.right);
+        
+        if (left == -1 || right == -1) {
+            return -1;
+        }
+        
+        if (Math.abs(left - right) > 1) {
+            return -1;
+        }
+        
+        return Math.max(left, right) + 1;
     }
 }

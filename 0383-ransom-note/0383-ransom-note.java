fb0a37b1734e308400d5646f813d5c9f5ffb1257
Kyung Soo "Andy" Min <counterchord89@gmail.com>
2024-03-06 23:05:02 +0900

    Time: 0 ms (100.00%), Space: 44.5 MB (78.76%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.java
+++ 0383-ransom-note/0383-ransom-note.java
@@ -1,25 +1,41 @@
+// class Solution {
+//     public boolean canConstruct(String ransomNote, String magazine) {
+//         if (ransomNote.length() > magazine.length()) {
+//             return false;
+//         }
+
+//         if (ransomNote.equals(magazine)) {
+//             return true;
+//         }
+
+//         int[] alphabets_counter = new int[26];
+
+//         for (char c : magazine.toCharArray()) {
+//             alphabets_counter[c - 'a']++;
+//         }
+
+//         for (char c : ransomNote.toCharArray()){
+//             if (alphabets_counter[c-'a'] == 0) {
+//                 return false;
+//             }
+
+//             alphabets_counter[c-'a']--;
+//         }
+
+//         return true;
+//     }
+// }
+
 class Solution {
     public boolean canConstruct(String ransomNote, String magazine) {
-        if (ransomNote.length() > magazine.length()) {
-            return false;
-        }
+       int[] alphabet = new int[26];
 
-        if (ransomNote.equals(magazine)) {
-            return true;
-        }
-
-        int[] alphabets_counter = new int[26];
-
-        for (char c : magazine.toCharArray()) {
-            alphabets_counter[c - 'a']++;
-        }
-
-        for (char c : ransomNote.toCharArray()){
-            if (alphabets_counter[c-'a'] == 0) {
+        for (char c : ransomNote.toCharArray()) {
+            int i = magazine.indexOf(c, alphabet[c-'a']);
+            if (i == -1) {
                 return false;
             }
-
-            alphabets_counter[c-'a']--;
+            alphabet[c-'a'] = i + 1;
         }
 
         return true;

60cd6415126d698d76509804c1b64d1fb9c7af0b
Kyung Soo "Andy" Min <counterchord89@gmail.com>
2024-03-06 22:59:23 +0900

    Time: 13 ms (38.37%), Space: 44.9 MB (49.90%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.java
+++ 0383-ransom-note/0383-ransom-note.java
@@ -1,42 +1,25 @@
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
 class Solution {
     public boolean canConstruct(String ransomNote, String magazine) {
-        // if (ransomNote.length() > magazine.length()) {
-        //     return false;
-        // }
-        // Pattern pattern = Pattern.compile(magazine.toString());
-        // Matcher matcher = pattern.matcher(ransomNote);
-        // if(matcher.find()){
-        //     return !matcher.find();
-        // }
-        // return false;
-        // HashMap<Character, Integer> result = new HashMap<>();
-        // for(char c : ransomNote.toCharArray()){
-        //     result.put(c,result.getOrDefault(c,0)+1);
-        // }
-        // for(char c : magazine.toCharArray()){
-        //     if(result.containsKey(c)){
-        //         result.put(c,result.getOrDefault(c,0)-1);
-        //     }
-        // }
-        // for(int val : result.values()){
-        //     if(val > 0){
-        //         return false;
-        //     }
-        // }
-        // return true;
-        if (ransomNote.length() > magazine.length()) return false;
-        int[] alphabets_counter = new int[26];
-        
-        for (char c : magazine.toCharArray())
-            alphabets_counter[c-'a']++;
-
-        for (char c : ransomNote.toCharArray()){
-            if (alphabets_counter[c-'a'] == 0) return false;
-            alphabets_counter[c-'a']--;
+        if (ransomNote.equals(magazine)) {
+            return true;
         }
+
+        Map<Character, Integer> map = new HashMap<>();
+
+        char[] charArray = magazine.toCharArray();
+        for (char c : charArray) {
+            map.merge(c, 1, Integer::sum);
+        }
+
+        char[] charArray1 = ransomNote.toCharArray();
+        for (char c : charArray1) {
+            if (map.get(c) == null || map.get(c) == 0) {
+                return false;
+            }
+
+            map.merge(c, -1, Integer::sum);
+        }
+
         return true;
     }
 }

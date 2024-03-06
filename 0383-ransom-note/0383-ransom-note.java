e918ba4b3d68ceee2f6a030113243fd66e7f7c09
Kyung Soo "Andy" Min <counterchord89@gmail.com>
2024-03-06 23:02:30 +0900

    Time: 2 ms (81.87%), Space: 44.5 MB (78.76%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.java
+++ 0383-ransom-note/0383-ransom-note.java
@@ -1,23 +1,25 @@
 class Solution {
     public boolean canConstruct(String ransomNote, String magazine) {
+        if (ransomNote.length() > magazine.length()) {
+            return false;
+        }
+
         if (ransomNote.equals(magazine)) {
             return true;
         }
 
-        Map<Character, Integer> map = new HashMap<>();
+        int[] alphabets_counter = new int[26];
 
-        char[] charArray = magazine.toCharArray();
-        for (char c : charArray) {
-            map.merge(c, 1, Integer::sum);
+        for (char c : magazine.toCharArray()) {
+            alphabets_counter[c - 'a']++;
         }
 
-        char[] charArray1 = ransomNote.toCharArray();
-        for (char c : charArray1) {
-            if (map.get(c) == null || map.get(c) == 0) {
+        for (char c : ransomNote.toCharArray()){
+            if (alphabets_counter[c-'a'] == 0) {
                 return false;
             }
 
-            map.merge(c, -1, Integer::sum);
+            alphabets_counter[c-'a']--;
         }
 
         return true;

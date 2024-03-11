a4f5a597acf8abba6b9f8c9ea6ac5ea41125ee4b
Kyung Soo "Andy" Min <counterchord89@gmail.com>
2024-03-11 18:15:46 +0900

    Time: 11 ms (75.42%), Space: 60 MB (36.16%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.java
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -13,26 +13,26 @@
 //     }
 // }
 
-class Solution {
-    public boolean containsDuplicate(int[] nums) {
-        HashMap<Integer, Integer> seen = new HashMap<>();
-        for (int num : nums) {
-            if (seen.containsKey(num) && seen.get(num) >= 1)
-                return true;
-            seen.put(num, seen.getOrDefault(num, 0) + 1);
-        }
-        return false;
-    }
-}
-
 // class Solution {
 //     public boolean containsDuplicate(int[] nums) {
-//         HashSet<Integer> seen = new HashSet<>();
+//         HashMap<Integer, Integer> seen = new HashMap<>();
 //         for (int num : nums) {
-//             if (seen.contains(num))
+//             if (seen.containsKey(num) && seen.get(num) >= 1)
 //                 return true;
-//             seen.add(num);
+//             seen.put(num, seen.getOrDefault(num, 0) + 1);
 //         }
 //         return false;
 //     }
 // }
+
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        HashSet<Integer> seen = new HashSet<>();
+        for (int num : nums) {
+            if (seen.contains(num))
+                return true;
+            seen.add(num);
+        }
+        return false;
+    }
+}
